5124892997006f8a5959ccdebf49b5407230fc3b
Daniel Shapiro <daniel@3dpasha.com>
2015-10-23 11:16:06 -0700

    changed onResponderConnected to onResponderInitialized

--- src/main/java/googleplus/Main.java
+++ src/main/java/googleplus/Main.java
@@ -32,8 +32,8 @@ public class Main extends DSLinkHandler {
 	}
 	
 	@Override
-	public void onResponderConnected(DSLink link){
-		LOGGER.info("Connected");
+	public void onResponderInitialized(DSLink link){
+		LOGGER.info("Initialized");
 		
 		NodeManager manager = link.getNodeManager();
         Node superRoot = manager.getNode("/").getNode();
